dd57a73470d773566882fe1db6989d4c61a41a90
manishrthingalaya <manish.t@robosoftin.com>
2022-12-15 15:55:16 +0530

    categories collection view

--- VirtualLearn/ViewControllers/Home/Categories/CategoryViewController.swift
+++ VirtualLearn/ViewControllers/Home/Categories/CategoryViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class CategoryViewController: UIViewController {
 
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
 
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
 
         
         super.viewDidLoad()
@@ -25,5 +29,28 @@ class CategoryViewController: UIViewController {
     }
     
 
+}
+
+extension CategoryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 11
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath)
+        
+        return item
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    
 }
 
