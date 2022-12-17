955b3654092a522c3f437c445669be8cf771d730
Sushruth <sushruth.hg@robosoftin.com>
2022-12-17 10:59:07 +0530

    chapter details api call

---
+++ VirtualLearn/Model/AssignmentResponse.swift
@@ -0,0 +1,28 @@
+//
+//  AssignmentResponse.swift
+//  VirtualLearn
+//
+//  Created by Sushruth H G on 16/12/22.
+//
+
+import Foundation
+
+
+class AssignmentResponse {
+    
+    var assignmentId : Int
+    var assignmentName: String
+    var testDuration : Int
+    var questionCount: Int
+    var grade : Int
+    
+    init(assignmentId : Int, assignmentName: String, testDuration : Int, questionCount: Int, grade : Int) {
+        
+        self.assignmentId = assignmentId
+        self.assignmentName = assignmentName
+        self.testDuration = testDuration
+        self.questionCount = questionCount
+        self.grade = grade
+        
+    }
+}
