59baa1b133ae99a9afed294c98521afa1bcd28cc
manishrthingalaya <manish.t@robosoftin.com>
2022-12-19 19:37:38 +0530

    Category cell design

---
+++ VirtualLearn/ViewControllers/Home/NewUserHomeViewController/CategoriesCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoriesCellCollectionViewCell.swift
+//  VirtualLearn
+//
+//  Created by Manish R T on 19/12/22.
+//
+
+import UIKit
+
+class CategoriesCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
