5870ba6b69fd2dbc58e3a0182cb88d2edb80ee85
Anushree JC <anushree.jc@robosoftin.com>
2022-12-19 18:08:58 +0530

    Edit Profile api call

---
+++ VirtualLearn/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditProfileViewModel.swift
+//  VirtualLearn
+//
+//  Created by Anushree J C on 19/12/22.
+//
+
+import Foundation
+class EditProfileViewModel {
+    let networkManager = NetworkManager()
+    
+    
+    
+}
