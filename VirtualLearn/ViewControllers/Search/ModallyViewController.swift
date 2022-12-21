1ff9375c18b87fcc3ad23764c64c519821cfc466
Sushruth <sushruth.hg@robosoftin.com>
2022-12-21 16:00:14 +0530

    search api call

--- VirtualLearn/ViewControllers/Search/ModallyViewController.swift
+++ VirtualLearn/ViewControllers/Search/ModallyViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ModallyViewController: UIViewController {
     
+    let shared = mainViewModel.mainShared
+    
+    var searchField = ""
+    
     
     @IBOutlet weak var designBtn: CustomBorderButton!
     @IBOutlet weak var developmentBtn: CustomBorderButton!
@@ -51,6 +55,15 @@ class ModallyViewController: UIViewController {
     @IBOutlet weak var twentyThirtyBtn: CustomBorderButton!
     @IBOutlet weak var thirtyFourtyBtn: CustomBorderButton!
     @IBOutlet weak var fourtyFiftyBtn: CustomBorderButton!
+    
+    
+    
+    
+    let duration1 = ["stratDuration" : 5, "EndDuration" : 10]
+    let duration2 = ["stratDuration" : 10, "EndDuration" : 20]
+    let duration3 = ["stratDuration" : 20, "EndDuration" : 30]
+    let duration4 = ["stratDuration" : 30, "EndDuration" : 40]
+    let duration5 = ["stratDuration" : 40, "EndDuration" : 50]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,10 +74,14 @@ class ModallyViewController: UIViewController {
         
         if isdesign{
             designBtn.selected()
+            getCategoryID(currentTitle: designBtn.currentTitle ?? "")
             isdesign = !isdesign
         }
         else if !isdesign{
             designBtn.deselected()
+            let id = returnCategoryID(currentTitle: designBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
+
             isdesign = !isdesign
         }
         
@@ -73,10 +90,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickDevelopment(_ sender: Any) {
         if isdevelopment{
             developmentBtn.selected()
+            getCategoryID(currentTitle: developmentBtn.currentTitle ?? "")
             isdevelopment = !isdevelopment
         }
         else if !isdevelopment{
             developmentBtn.deselected()
+            let id = returnCategoryID(currentTitle: developmentBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isdevelopment = !isdevelopment
         }
     }
@@ -84,10 +104,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickBusiness(_ sender: Any) {
         if isbusiness{
             businessBtn.selected()
+            getCategoryID(currentTitle: businessBtn.currentTitle ?? "")
             isbusiness = !isbusiness
         }
         else if !isbusiness{
             businessBtn.deselected()
+            let id = returnCategoryID(currentTitle: businessBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isbusiness = !isbusiness
         }
     }
@@ -95,10 +118,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickFinance(_ sender: Any) {
         if isfinance{
             financeBtn.selected()
+            getCategoryID(currentTitle: financeBtn.currentTitle ?? "")
             isfinance = !isfinance
         }
         else if !isfinance{
             financeBtn.deselected()
+            let id = returnCategoryID(currentTitle: financeBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isfinance = !isfinance
         }
     }
@@ -106,10 +132,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickHealthFitness(_ sender: Any) {
         if ishealth{
             healthFitnessBtn.selected()
+            getCategoryID(currentTitle: healthFitnessBtn.currentTitle ?? "")
             ishealth = !ishealth
         }
         else if !ishealth{
             healthFitnessBtn.deselected()
+            let id = returnCategoryID(currentTitle: healthFitnessBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             ishealth = !ishealth
         }
     }
@@ -117,10 +146,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickMusic(_ sender: Any) {
         if ismusic{
             musicBtn.selected()
+            getCategoryID(currentTitle: musicBtn.currentTitle ?? "")
             ismusic = !ismusic
         }
         else if !ismusic{
             musicBtn.deselected()
+            let id = returnCategoryID(currentTitle: musicBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             ismusic = !ismusic
         }
     }
@@ -128,10 +160,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickIT(_ sender: Any) {
         if isIT{
             ITBtn.selected()
+            getCategoryID(currentTitle: ITBtn.currentTitle ?? "")
             isIT = !isIT
         }
         else if !isIT{
             ITBtn.deselected()
+            let id = returnCategoryID(currentTitle: ITBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isIT = !isIT
         }
     }
@@ -139,10 +174,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickMarketing(_ sender: Any) {
         if ismarketing{
             marketingBtn.selected()
+            getCategoryID(currentTitle: marketingBtn.currentTitle ?? "")
             ismarketing = !ismarketing
         }
         else if !ismarketing{
             marketingBtn.deselected()
+            let id = returnCategoryID(currentTitle: marketingBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             ismarketing = !ismarketing
         }
     }
@@ -150,10 +188,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickLifestyle(_ sender: Any) {
         if islifestyle{
             lifeStyleBtn.selected()
+            getCategoryID(currentTitle: lifeStyleBtn.currentTitle ?? "")
             islifestyle = !islifestyle
         }
         else if !islifestyle{
             lifeStyleBtn.deselected()
+            let id = returnCategoryID(currentTitle: lifeStyleBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             islifestyle = !islifestyle
         }
     }
@@ -161,10 +202,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickPhotography(_ sender: Any) {
         if isphotography{
             PhotographyBtn.selected()
+            getCategoryID(currentTitle: PhotographyBtn.currentTitle ?? "")
             isphotography = !isphotography
         }
         else if !isphotography{
             PhotographyBtn.deselected()
+            let id = returnCategoryID(currentTitle: PhotographyBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isphotography = !isphotography
         }
     }
@@ -172,10 +216,13 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickTeaching(_ sender: Any) {
         if isteaching{
             teachingBtn.selected()
+            getCategoryID(currentTitle: teachingBtn.currentTitle ?? "")
             isteaching = !isteaching
         }
         else if !isteaching{
             teachingBtn.deselected()
+            let id = returnCategoryID(currentTitle: teachingBtn.currentTitle ?? "")
+            shared.searchViewModelShared.categories = shared.searchViewModelShared.categories.filter{$0 != id}
             isteaching = !isteaching
         }
     }
@@ -183,10 +230,12 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickFiveTen(_ sender: Any) {
         if isFiveTen{
             fiveTenBtn.selected()
+            shared.searchViewModelShared.duration.append(duration1)
             isFiveTen = !isFiveTen
         }
         else if !isFiveTen{
             fiveTenBtn.deselected()
+            shared.searchViewModelShared.duration = shared.searchViewModelShared.duration.filter{$0 != duration1}
             isFiveTen = !isFiveTen
         }
     }
@@ -194,10 +243,12 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickTenTwenty(_ sender: Any) {
         if isTenTwenty{
             tenTwentyBtn.selected()
+            shared.searchViewModelShared.duration.append(duration2)
             isTenTwenty = !isTenTwenty
         }
         else if !isTenTwenty{
             tenTwentyBtn.deselected()
+            shared.searchViewModelShared.duration = shared.searchViewModelShared.duration.filter{$0 != duration2}
             isTenTwenty = !isTenTwenty
         }
     }
@@ -205,10 +256,12 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickTwentyThirty(_ sender: Any) {
         if isTwentyThirty{
             twentyThirtyBtn.selected()
+            shared.searchViewModelShared.duration.append(duration3)
             isTwentyThirty = !isTwentyThirty
         }
         else if !isTwentyThirty{
             twentyThirtyBtn.deselected()
+            shared.searchViewModelShared.duration = shared.searchViewModelShared.duration.filter{$0 != duration3}
             isTwentyThirty = !isTwentyThirty
         }
     }
@@ -216,10 +269,12 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickThirtyFourty(_ sender: Any) {
         if isThirtyFourty{
             thirtyFourtyBtn.selected()
+            shared.searchViewModelShared.duration.append(duration4)
             isThirtyFourty = !isThirtyFourty
         }
         else if !isThirtyFourty{
             thirtyFourtyBtn.deselected()
+            shared.searchViewModelShared.duration = shared.searchViewModelShared.duration.filter{$0 != duration4}
             isThirtyFourty = !isThirtyFourty
         }
     }
@@ -228,15 +283,75 @@ class ModallyViewController: UIViewController {
     @IBAction func onClickFourtyFifty(_ sender: Any) {
         if isFourtyFifty{
             fourtyFiftyBtn.selected()
+            shared.searchViewModelShared.duration.append(duration5)
             isFourtyFifty = !isFourtyFifty
         }
         else if !isFourtyFifty{
             fourtyFiftyBtn.deselected()
+            shared.searchViewModelShared.duration = shared.searchViewModelShared.duration.filter{$0 != duration5}
             isFourtyFifty = !isFourtyFifty
         }
     }
     
     
+    @IBAction func onClickApplyFilter(_ sender: Any) {
+        shared.searchViewModelShared.searchOption = searchField
+        shared.searchViewModelShared.getSearchResult { (result) in
+            print(result)
+        } fail: { (fail) in
+            print(fail)
+        }
+        shared.searchViewModelShared.duration.removeAll()
+        shared.searchViewModelShared.categories.removeAll()
+        dismiss(animated: true, completion: nil)
+
+        
+    }
+    
+    
+    @IBAction func onClickClearAll(_ sender: Any) {
+        
+        designBtn.deselected()
+        developmentBtn.deselected()
+        businessBtn.deselected()
+        financeBtn.deselected()
+        healthFitnessBtn.deselected()
+        musicBtn.deselected()
+        ITBtn.deselected()
+        marketingBtn.deselected()
+        lifeStyleBtn.deselected()
+        PhotographyBtn.deselected()
+        teachingBtn.deselected()
+        
+        fiveTenBtn.deselected()
+        tenTwentyBtn.deselected()
+        twentyThirtyBtn.deselected()
+        thirtyFourtyBtn.deselected()
+        fourtyFiftyBtn.deselected()
+        
+ 
+    }
+    
+    
+    
+    
+    func getCategoryID(currentTitle: String){
+        print("function called")
+        for i in shared.categoriesViewModelShared.listofCategories{
+            if currentTitle == i.categotyName {
+                shared.searchViewModelShared.categories.append(Int(i.categoryId) ?? 0)
+            }
+        }
+    }
+    func returnCategoryID(currentTitle: String) -> Int{
+        var temp = 0
+        for i in shared.categoriesViewModelShared.listofCategories{
+            if currentTitle == i.categotyName {
+                temp = Int(i.categoryId) ?? 0
+            }
+        }
+        return temp
+    }
     
     
 }
--- VirtualLearn/ViewModel/searchViewModel.swift
+++ VirtualLearn/ViewModel/searchViewModel.swift
@@ -10,20 +10,25 @@ import Foundation
 class SearchViewModel {
     
     let networkManeger = NetWorkManager()
+    
+    
+    var duration = [[String : Int]]()
+    var searchOption = ""
+    var categories = [Int]()
 
     
     func getSearchResult(completion: @escaping (Bool) -> (), fail: @escaping (Bool) ->()){
         let url = URL(string: "https://app-virtuallearning-221207091853.azurewebsites.net/user/view/search")!
         var request = URLRequest(url: url)
-        request.httpMethod = "GET"
+        request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
 
         let parameters: [String : Any] = [
             
-            "searchOption" : "design"
-//            "categories" : [1,2],
-//            "durationRequestList":[["startDuration" : 5,"endDuration" : 10],["startDuration" : 15,"endDuration" : 20]]
+            "searchOption" : searchOption,
+            "categories" : categories,
+            "durationRequestList": duration
         ]
         print(parameters)
        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .fragmentsAllowed)
