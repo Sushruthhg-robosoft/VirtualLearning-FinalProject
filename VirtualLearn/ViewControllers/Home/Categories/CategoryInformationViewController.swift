3f2e668e9ca451efa449e82d6123a8df7237efc9
Sushruth <sushruth.hg@robosoftin.com>
2022-12-26 12:55:04 +0530

    Font change

--- VirtualLearn/ViewControllers/Home/Categories/CategoryInformationViewController.swift
+++ VirtualLearn/ViewControllers/Home/Categories/CategoryInformationViewController.swift
@@ -26,7 +26,7 @@ class CategoryInformationViewController: UIViewController{
         
         initCollectionViewForTop(collectionView: courseToGetStartedCollectionView)
         initCollectionViewForTop(collectionView: featureCourseCollectionView)
-        initCollectionViewForsubCategory(collectionView:subCategoryCollectionView)
+//        initCollectionViewForsubCategory(collectionView:subCategoryCollectionView)
         
         CategoryLabel.text = categoryName
         let loader = self.loader()
@@ -110,16 +110,16 @@ class CategoryInformationViewController: UIViewController{
         collectionView.dataSource = self
         collectionView.delegate = self
     }
-    private func initCollectionViewForsubCategory(collectionView: UICollectionView) {
-        let nib = UINib(nibName: "SubCategoriesCollectionViewCell", bundle: nil)
-        collectionView.register(nib, forCellWithReuseIdentifier: "subCategoriesCell")
-        collectionView.dataSource = self
-        collectionView.delegate = self
-    }
+//    private func initCollectionViewForsubCategory(collectionView: UICollectionView) {
+//        let nib = UINib(nibName: "SubCategoriesCollectionViewCell", bundle: nil)
+//        collectionView.register(nib, forCellWithReuseIdentifier: "subCategoriesCell")
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+//    }
     
     
 }
-extension CategoryInformationViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+extension CategoryInformationViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch collectionView {
         case courseToGetStartedCollectionView :
@@ -182,6 +182,8 @@ extension CategoryInformationViewController: UICollectionViewDelegate, UICollect
         navigationController?.pushViewController(vc, animated: true)
     }
     
+  
+    
 }
 
 
@@ -209,42 +211,42 @@ extension CategoryInformationViewController: UITableViewDataSource, UITableViewD
     }
 }
 
-class FlowLayout: UICollectionViewFlowLayout {
+//class FlowLayout: UICollectionViewFlowLayout {
+//
+//    required init(minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+//        super.init()
+//
+//        estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+//        self.minimumInteritemSpacing = minimumInteritemSpacing
+//        self.minimumLineSpacing = minimumLineSpacing
+//        self.sectionInset = sectionInset
+//        sectionInsetReference = .fromSafeArea
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+//        let layoutAttributes = super.layoutAttributesForElements(in: rect)!.map { $0.copy() as! UICollectionViewLayoutAttributes }
+//        guard scrollDirection == .vertical else { return layoutAttributes }
+//
+//        // Filter attributes to compute only cell attributes
+//        let cellAttributes = layoutAttributes.filter({ $0.representedElementCategory == .cell })
+//
+//        // Group cell attributes by row (cells with same vertical center) and loop on those groups
+//        for (_, attributes) in Dictionary(grouping: cellAttributes, by: { ($0.center.y / 10).rounded(.up) * 10 }) {
+//            // Set the initial left inset
+//            var leftInset = sectionInset.left
+//
+//            // Loop on cells to adjust each cell's origin and prepare leftInset for the next cell
+//            for attribute in attributes {
+//                attribute.frame.origin.x = leftInset
+//                leftInset = attribute.frame.maxX + minimumInteritemSpacing
+//            }
+//        }
+//
+//        return layoutAttributes
+//    }
 
-    required init(minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
-        super.init()
-
-        estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-        self.minimumInteritemSpacing = minimumInteritemSpacing
-        self.minimumLineSpacing = minimumLineSpacing
-        self.sectionInset = sectionInset
-        sectionInsetReference = .fromSafeArea
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let layoutAttributes = super.layoutAttributesForElements(in: rect)!.map { $0.copy() as! UICollectionViewLayoutAttributes }
-        guard scrollDirection == .vertical else { return layoutAttributes }
-
-        // Filter attributes to compute only cell attributes
-        let cellAttributes = layoutAttributes.filter({ $0.representedElementCategory == .cell })
-
-        // Group cell attributes by row (cells with same vertical center) and loop on those groups
-        for (_, attributes) in Dictionary(grouping: cellAttributes, by: { ($0.center.y / 10).rounded(.up) * 10 }) {
-            // Set the initial left inset
-            var leftInset = sectionInset.left
-
-            // Loop on cells to adjust each cell's origin and prepare leftInset for the next cell
-            for attribute in attributes {
-                attribute.frame.origin.x = leftInset
-                leftInset = attribute.frame.maxX + minimumInteritemSpacing
-            }
-        }
-
-        return layoutAttributes
-    }
-
-}
+//}
