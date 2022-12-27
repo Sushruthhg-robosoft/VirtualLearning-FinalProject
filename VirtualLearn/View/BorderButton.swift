30b50ba4966a73e059aedceaf146e7a7eb93f533
manishrthingalaya <manish.t@robosoftin.com>
2022-12-27 16:20:46 +0530

    UIChanges

--- VirtualLearn/View/BorderButton.swift
+++ VirtualLearn/View/BorderButton.swift
@@ -22,7 +22,8 @@ class CustomBorderButton: UIButton {
     func addBorder() {
 //        self.titleEdgeInsets = UIEdgeInsets(top: 0,left: 5,bottom: 0,right: 5)
         self.layer.cornerRadius = 6
-        self.layer.borderWidth = 1
+        self.layer.borderWidth = 0.5
         self.layer.borderColor = UIColor(red: 122/255, green: 122/255, blue: 122/255, alpha: 1).cgColor
+        self.imageEdgeInsets = UIEdgeInsets(top: 1, left: 1, bottom: 1, right: 0)
     }
 }
