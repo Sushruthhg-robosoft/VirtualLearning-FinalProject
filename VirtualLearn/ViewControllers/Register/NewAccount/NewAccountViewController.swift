f9a2e4dc7e0a59804bbd3aef1a776ea0464c9e23
Anushree JC <anushree.jc@robosoftin.com>
2022-12-27 10:18:27 +0530

    keyboard handling for change your password, create your password,, New account view controller

--- VirtualLearn/ViewControllers/Register/NewAccount/NewAccountViewController.swift
+++ VirtualLearn/ViewControllers/Register/NewAccount/NewAccountViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewAccountViewController: UIViewController {
+class NewAccountViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var enteredMobileNumber: UITextField!
     
@@ -17,10 +17,11 @@ class NewAccountViewController: UIViewController {
         super.viewDidLoad()
         initializeHideKeyboard()
         
+        enteredMobileNumber.delegate = self
+        
         if(view.bounds.height > 500) {
             
             viewHeight.constant = view.bounds.height - 50
-            
         }
         enteredMobileNumber.removeBorder()
         
@@ -61,8 +62,16 @@ class NewAccountViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
-}
 
+
+func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == enteredMobileNumber {
+            textField.resignFirstResponder()
+            
+        }
+    return true
+}
+}
 
 extension NewAccountViewController {
 func initializeHideKeyboard(){
@@ -76,8 +85,6 @@ func initializeHideKeyboard(){
     @objc func dismissMyKeyboard(){
         view.endEditing(true)
     }
-    
-
-
 }
+
 
